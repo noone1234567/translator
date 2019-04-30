a73e71f1a5b00c515566d62fd2e840c5b9725b9b
noone1234567 <46103469+noone1234567@users.noreply.github.com>
2019-04-30 22:03:05 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -5,7 +5,7 @@ import json
 
 def translate(word):
     lang = "en"
-    key = "trnsl.1.1.20190430T184245Z.d64c8ccf0b01b8d1.8221599076bea49f91cbc451af19b77fdcc703e2" # мой ключ
+    key = "trnsl.1.1.20190430T184245Z.d64c8ccf0b01b8d1.8221599076bea49f91cbc451af19b77fdcc703e2" # УТРОМ ДОДЕЛАЮ ЧЕСТНО
     req = "https://translate.yandex.net/api/v1.5/tr.json/translate?key={}&text={}&lang={}".format(key, word, lang)
 
     response = requests.get(req)
