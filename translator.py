f73ce4c4d504d5cc31aa53b5a40af9f36a18d0bb
noone1234567 <46103469+noone1234567@users.noreply.github.com>
2019-05-01 12:35:19 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,11 +1,53 @@
 from flask import Flask, request
-import requests
+import logging
 import json
+import random
+
+#https://www.pythonanywhere.com/user/alwin123/files/home/alwin123
+app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info('Request: %r', request.json)
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+        }
+    }
+    handle_dialog(response, request.json)
+    logging.info('Response: %r', response)
+    return json.dumps(response)
+
+
+def handle_dialog(res, req):
+    user_id = req['session']['user_id']
+    res['response']['buttons'] = [{'title': 'Помощь', 'hide': True},
+                                  {'title': 'http://translate.yandex.ru/', 'hide': True, 'url': 'http://translate.yandex.ru/'}]
+    logging.info(req['request']['command'])
+    if req['session']['new']:
+        res['response']['text'] = 'Это переводчик отдельных слов. Формат запроса: "Переведите (переведи) слово: *слово*". Переведено сервисом «Яндекс.Переводчик»'
+        return
+
+    if 'помощь' in req['request']['nlu']['tokens'] or 'помоги' in req['request']['nlu']['tokens']:
+        res['response']['text'] = 'Это переводчик отдельных слов. Формат запроса: "Переведите (переведи) слово: *слово*". Переведено сервисом «Яндекс.Переводчик». Поехали!'
+        return
+
+    if req['request']['nlu']['tokens'][0] in ['переведи', 'переведите'] and len(req['request']['nlu']['tokens']) == 3 and req['request']['nlu']['tokens'][1] == 'слово':
+        text = translate(req['request']['nlu']['tokens'][2])
+        res['response']['text'] = 'Ошибка' if text is None else text
+
+        return
+    else:
+         res['response']['text'] = 'Неправильный формат запроса. Попробуй ещё раз.'
+
+    return
 
 
 def translate(word):
     lang = "en"
-    key = "trnsl.1.1.20190430T184245Z.d64c8ccf0b01b8d1.8221599076bea49f91cbc451af19b77fdcc703e2" # УТРОМ ДОДЕЛАЮ ЧЕСТНО
+    key = "trnsl.1.1.20190430T184245Z.d64c8ccf0b01b8d1.8221599076bea49f91cbc451af19b77fdcc703e2"
     req = "https://translate.yandex.net/api/v1.5/tr.json/translate?key={}&text={}&lang={}".format(key, word, lang)
 
     response = requests.get(req)
@@ -14,4 +56,5 @@ def translate(word):
 
 
 if __name__ == '__main__':
-    print(translate('коробка'))
+    app.run()
+
